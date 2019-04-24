a2a3acb492a7d68ea1876ddf3c2fb98182970ea5
Angela <angela.novoa.ramos@alumnos.upm.es>
2019-04-24 15:04:58 +0100

    Working bot with nlp

--- bot/axa-bot/bot.js
+++ bot/axa-bot/bot.js
@@ -1,21 +1,24 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-const card = require('./Cards/card');
-const { ActivityTypes, CardFactory } = require('botbuilder');
+const { ActivityTypes } = require('botbuilder');
+const { Recognizer } = require('./lib');
+const trainnlp = require('./train-nlp');
 
 class MyBot {
-    /**
-     *
-     * @param {TurnContext} on turn context object.
-     */
-    async onTurn(turnContext) {
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
-        } else {
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
-        }
+  constructor() {
+    this.recognizer = new Recognizer();
+    this.recognizer.nlpManager.addLanguage('en');
+    trainnlp(this.recognizer.nlpManager, console.log);
+  }
+
+  async onTurn(turnContext) {
+    if (turnContext.activity.type === ActivityTypes.Message) {
+      try {
+        const result = await this.recognizer.recognize(turnContext);
+        await turnContext.sendActivity(result.answer);
+      } catch (ex) {
+        await turnContext.sendActivity('Opps! there was an error recognizing!');
+      }
     }
+  }
 }
 
-module.exports.MyBot = MyBot;
+module.exports = MyBot;
