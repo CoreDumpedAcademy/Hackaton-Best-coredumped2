d86295a30a78b13d999aeba3e08581b3f895a3bf
TebionDL <purreren@gmail.com>
2019-04-24 11:30:19 +0200

    add new card

--- bot/axa-bot/bot.js
+++ bot/axa-bot/bot.js
@@ -1,13 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-//Importar Adaptive
-var AdaptiveCards = require("adaptivecards");
 const card = require('./Cards/card');
-
-const { ActivityTypes } = require('botbuilder');
+const { ActivityTypes, CardFactory } = require('botbuilder');
 
 class MyBot {
-
     /**
      *
      * @param {TurnContext} on turn context object.
@@ -15,9 +11,7 @@ class MyBot {
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity({ 
-                attachments: [ActivityTypes.adaptiveCard(card.card)]  
-            });
+            await turnContext.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
         } else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }
