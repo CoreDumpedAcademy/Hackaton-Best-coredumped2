f5384c11ae83077a39b09f0020676b22d1999a1f
TebionDL <purreren@gmail.com>
2019-04-24 15:38:49 +0200

    minor fixes

--- bot/axa-bot/index.js
+++ bot/axa-bot/index.js
@@ -8,18 +8,18 @@ const myBot = new MyBot();
 const app = express();
 
 const adapter = new BotFrameworkAdapter({
-  appId: process.env.BOT_APP_ID,
-  appPassword: process.env.BOT_APP_PASSWORD,
+    appId: process.env.BOT_APP_ID,
+    appPassword: process.env.BOT_APP_PASSWORD
 });
 
 adapter.onTurnError = async context => {
-  await context.sendActivity('Oops. Something went wrong!');
+    await context.sendActivity('Oops. Something went wrong!');
 };
 
 app.post('/api/messages', (req, res) => {
-  adapter.processActivity(req, res, async context => {
-    await myBot.onTurn(context);
-  });
+    adapter.processActivity(req, res, async context => {
+        await myBot.onTurn(context);
+    });
 });
 
 app.listen(process.env.port || process.env.PORT || 3978);
